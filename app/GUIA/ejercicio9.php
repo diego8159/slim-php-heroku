386e6efb483e617d8482bae5b0f72d81f6ec890a
diego8159 <diegojmv59@gmail.com>
2021-03-23 21:55:25 -0300

    Modificando ejercicio 9

--- app/GUIA/ejercicio9.php
+++ app/GUIA/ejercicio9.php
@@ -8,6 +8,11 @@ lapiceras.
 */
 echo "<br>EJERCICIO 9<br><br>";
 
+$color = '';
+$marca = '';
+$trazo = 0;
+$precio = 0;
+
 $lapicera[0] = array($color => 'blue', $marca => 'Zanst Kort', $trazo => 2, $precio => 76);
 
 $lapicera[1] = array($color => 'red', $marca => 'Bic', $trazo => 3, $precio => 110);
@@ -18,7 +23,6 @@ $i =0;
 foreach($lapicera[$i] as $clave=>$valor)
 	{
 	echo " " . $clave . " " . $valor . "<br/>";
-	
 	}
 	$i = $i + 1;
 
