a9e7d76e6e9062f9f394a1ec9d4708eb8479374b
diego8159 <diegojmv59@gmail.com>
2021-03-26 17:01:25 -0300

    Ejercicio 15 Prueba 2

--- app/GUIA/ejercicio15/index.php
+++ app/GUIA/ejercicio15/index.php
@@ -10,16 +10,18 @@ funcionalidad será la de inicializar los atributos _superficie y _perimetro.
 Dibujar, retornará un string (con el color que corresponda) formando la figura geométrica del
 objeto que lo invoque (retornar una serie de asteriscos que modele el objeto).
 */
-    require"FiguraGeometrica.php";
-    require"Rectangulo.php";
-    require"Triangulo.php";
+    require "FiguraGeometrica.php";
+    require "Rectangulo.php";
+    require "Triangulo.php";
 
     $miTriangulo= new Triangulo(2,3);
-    var_dump($miTriangulo);
+    //var_dump($miTriangulo);
+    echo "Triangulo: <br>";
     Triangulo::Dibujar();
 
     $miRectangulo= new Rectangulo(1,4);
-    var_dump($miRectangulo);
+    //var_dump($miRectangulo);
+    echo "Rectangulo: <br>";
     Rectangulo::ToString();
     $miRectangulo->Dibujar();
 
