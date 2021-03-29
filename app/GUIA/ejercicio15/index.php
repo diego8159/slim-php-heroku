c6f83cf051a021a4ec089193aa5f642dfad5955e
diego8159 <diegojmv59@gmail.com>
2021-03-29 12:23:59 -0300

    Ejercicio 15 Prueba 19

--- app/GUIA/ejercicio15/index.php
+++ app/GUIA/ejercicio15/index.php
@@ -11,7 +11,7 @@ Dibujar, retornará un string (con el color que corresponda) formando la figura 
 objeto que lo invoque (retornar una serie de asteriscos que modele el objeto).
 */
     include "./FiguraGeometrica.php";
-    //include "./Rectangulo.php";
+    include "./Rectangulo.php";
     include "./Triangulo.php";
 
     $miTriangulo= new Triangulo(2,3);
@@ -19,14 +19,14 @@ objeto que lo invoque (retornar una serie de asteriscos que modele el objeto).
     echo "Triangulo: <br>";
     //Triangulo::ToString();
     $miTriangulo->Dibujar();
-    echo "Triangulo vs2: <br>";
+    //echo "Triangulo vs2: <br>";
     //Triangulo::Dibujar();
 
     //$miRectangulo= new Rectangulo(1,4);
     //var_dump($miRectangulo);
     echo "Rectangulo: <br>";
     //Rectangulo::ToString();
-    //$miRectangulo->Dibujar();
+    $miRectangulo->Dibujar();
 
 
 ?>
