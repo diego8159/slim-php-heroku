c93c326c9410e400b07203cf828e77d0cfda0ddc
diego8159 <diegojmv59@gmail.com>
2021-03-29 10:34:12 -0300

    Ejercicio 15 Prueba 9

--- app/GUIA/ejercicio15/index.php
+++ app/GUIA/ejercicio15/index.php
@@ -17,13 +17,13 @@ objeto que lo invoque (retornar una serie de asteriscos que modele el objeto).
     $miTriangulo= new Triangulo(2,3);
     //var_dump($miTriangulo);
     echo "Triangulo: <br>";
-    //Triangulo::Dibujar();
-    $miTriangulo::Dibujar();
+    Triangulo::ToString();
+    $miTriangulo->Dibujar();
 
     $miRectangulo= new Rectangulo(1,4);
     //var_dump($miRectangulo);
     echo "Rectangulo: <br>";
-    //Rectangulo::ToString();
+    Rectangulo::ToString();
     $miRectangulo->Dibujar();
 
 
